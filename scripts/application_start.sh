cfb7f0eedfb929aad0d81c5fb8d36cadec0e93c3
stykle <Maxamadabdulahist@outlook.com>
2024-10-01 17:18:18 +0200

    update

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,4 +12,4 @@ docker ps -a --filter "ancestor=stykle/brevo" --format "{{.ID}}" | xargs -r dock
 docker images 'stykle/brevo' -q | xargs -r docker rmi
 
 # Run the new container
-docker run -d --name brevoemail-sender -e EMAIL_USERNAME="$EMAIL_USERNAME"  -e EMAIL_PASSWORD="$EMAIL_PASSWORD"  -e SENDER_NAME="$SENDER_NAME"   -e SENDER_EMAIL="$SENDER_EMAIL"  -e BREVO_API_KEY="$BREVO_API_KEY" -p 80:8080 stykle/brevo:latestbrevoemail-sender
+docker run -d --name brevoemail-sender -e EMAIL_USERNAME="$EMAIL_USERNAME"  -e EMAIL_PASSWORD="$EMAIL_PASSWORD"  -e SENDER_NAME="$SENDER_NAME"   -e SENDER_EMAIL="$SENDER_EMAIL"  -e BREVO_API_KEY="$BREVO_API_KEY" -p 80:8080 stykle/brevo:latest
